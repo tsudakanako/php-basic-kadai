c8b5378b7f9b499b9f6ba4e357a18b40543e0fef
tsudakanako <167269340+tsudakanako@users.noreply.github.com>
2024-07-19 11:32:20 +0900

    kadai_005をアップロード

---
+++ kadai_005/score.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>課題5</title>
+ </head>
+ 
+ <body>
+    <p>
+      <?php
+        $score1 = 80;
+        $score2 = 60;
+        $score3 = 55;
+        $score4 = 40;
+        $score5 = 100;
+        $score6 = 25;
+        $score7 = 80;
+        $score8 = 95;
+        $score9 = 30;
+        $score10 = 60;
+        
+        $number = 80+60+55+40+100+25+80+95+30+60;
+        echo $number / 10;
+      ?>
+      
+      </p>
+
+</body>
+</html>
