ce6348ee3c37d487dad87536d3c62283f01f9a4e
tsudakanako <167269340+tsudakanako@users.noreply.github.com>
2024-07-21 01:31:24 +0900

    kadai_007をアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>課題7</title>
+ </head>
+ 
+ <body>
+     <p>
+        <?php
+        $product_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+        print_r($product_data);
+        ?>
+
+     </p>
+ </body>
+ 
+ </html>
